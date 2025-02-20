0a6bd301a560850c551532975fe61cc8cabe7314
1hiteshk <1hiteshk@gmail.com>
2025-02-20 21:03:52 +0530

    update : fetch limit of new connections to 10

--- src/routes/user.js
+++ src/routes/user.js
@@ -59,7 +59,7 @@ userRouter.get("/feed", userAuth, async (req, res) => {
 
     const page = parseInt(req?.query?.page) || 1;
    // console.log(page);
-    let limit = parseInt(req?.query?.limit) || 30;
+    let limit = parseInt(req?.query?.limit) || 10;  
     limit = limit > 50 ? 50 : limit;
     const skip = (page - 1) * limit;
 
